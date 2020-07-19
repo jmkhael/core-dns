a4abab7095f98a169e4cf8d3aac3d5941b752564
Johnny Mkhael <johnny.mkhael@gmail.com>
2020-07-19 14:41:52 +0200

    Removing restart flag

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d --name coredns --restart=always --volume=/d/1d-mx/workspace/coredns/:/root/ -p 1053:53/udp coredns/coredns -conf /root/Corefile
+docker run -d --name coredns --volume=/d/1d-mx/workspace/coredns/:/root/ -p 1053:53/udp coredns/coredns -conf /root/Corefile
